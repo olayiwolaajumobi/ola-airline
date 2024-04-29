340a24d774e44949ff94930e4ae65604ba33a707
olayiwola <kareemrongofho90@gmail.com>
2024-04-29 09:15:20 +0100

    initial commit

--- ticket_booking/urls.py
+++ ticket_booking/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, create_account, login_view, logout_view
 
 
 urlpatterns = [ 
     path("", index),
-    
+    path("create", create_account),
+    path("login", login_view),
+    path("logout", logout_view)
 ]
 
--- ticket_booking/views.py
+++ ticket_booking/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .models import FlightLocation
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 
@@ -10,5 +12,64 @@ def index (req):
     return render(req, "index.html", {
         "location": location
     })
+def create_account(req):
+    if req.method == "POST":
+        first_name = req.POST['first_name']
+        last_name = req.POST['last_name']
+        username = req.POST['username']
+        email = req.POST['email']
+        password = req.POST['password']
+        
+
+        try:
+            User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password
+                
+                )
+            return HttpResponseRedirect("/")
+        except:
+            print("error")
+            pass
+
+    return render(req, "create_account.html")
+
+
+def login_view(req):
+
+    if req.method == "POST":
+        username=req.POST["username"]
+        password=req.POST["password"]
+
+        try:
+            user= authenticate(username=username, password=password)
+
+            if user == None:
+                return render(req, "login.html"), {
+                    'error' : "wrong username or password"
+                }
+            else:
+                
+                login(req, user)
+                try:
+                    return HttpResponseRedirect(req.GET["next"])
+                except:
+                    return HttpResponseRedirect("/")
+        except:
+            pass
+
+    return render(req, "login.html")
+
+def logout_view(req):
+    logout(req)
+    return HttpResponseRedirect("/")
+
+
+
+
+
 
 
